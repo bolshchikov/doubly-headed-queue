d9a704e8c74c049cb35f3d3afa3561080a2e7482
bolshchikov <sergey@bolshchikov.net>
2012-12-15 04:16:11 +0200

    enqueue and dequeue improved

--- js/queue.js
+++ js/queue.js
@@ -1,39 +1,48 @@
-function AppView() {
-  this.value = '';
-  this.queue = ko.observableArray();
+function generateNumber() {
+  return Math.floor(Math.random()*101);
+}
 
-  this.create = function () {};
+function AppView() {
+
+  this.queues = ko.observableArray();
+
+  this.create = function () {
+    this.queues.push({array: ko.observableArray(), attemps: 0});
+    this.queues()[this.queues().length - 1].array.push('Hi, I\'m your new queue');
+  };
 
   this.enqueueHead = function () {
-    if (!this.value) {
-      alert('Please, enter the value');
-      return;
-    }
-    this.queue.unshift(this.value);
+    if (this.queues()[this.queues().length - 1].attemps === 0) {
+      this.queues()[this.queues().length - 1].array.pop();
+    };
+    this.queues()[this.queues().length - 1].array.unshift(generateNumber());
+    this.queues()[this.queues().length - 1].attemps += 1;
   };
   this.dequeueHead = function () {
-    if (!this.value) {
-      alert('Please, enter the value');
+    if (this.queues()[this.queues().length - 1].attemps === 0) {
+      alert('Please, append a member before dequeueing');
       return;
-    }
-    this.queue.shift(this.value);
+    };
+    this.queues()[this.queues().length - 1].array.shift();
   };
   this.enqueueTail = function () {
-    if (!this.value) {
-      alert('Please, enter the value');
-      return;
-    }
-    this.queue.push(this.value);
+     if (this.queues()[this.queues().length - 1].attemps === 0) {
+      this.queues()[this.queues().length - 1].array.pop();
+    };
+    this.queues()[this.queues().length - 1].array.push(generateNumber());
+    this.queues()[this.queues().length - 1].attemps += 1;
   };
   this.dequeueTail = function () {
-    if (!this.value) {
-      alert('Please, enter the value');
+    if (this.queues()[this.queues().length - 1].attemps === 0) {
+      alert('Please, append a member before dequeueing');
       return;
-    }
-    this.queue.pop(this.value);
+    };
+    this.queues()[this.queues().length - 1].array.pop();
   };
 
-  this.concate = function () {};
+  this.concat = function () {
+
+  };
 }
 
 ko.applyBindings(new AppView());
